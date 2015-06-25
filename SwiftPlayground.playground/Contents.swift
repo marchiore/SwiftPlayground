f49fdc48314f3b53902be4bcc3f4a81d1f21404d
matheus <marchiore.matheus@gmail.com>
2015-06-25 20:21:47 -0300

    map testD

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -3,3 +3,16 @@
 import UIKit
 
 var str = "Hello, playground"
+
+var array = [1, 2, 3, 4]
+
+func dobrar(numero: Int) -> Int{
+    return numero * 2
+}
+
+var novo = array.map { result in result * 2 }
+
+println(novo)
+
+
+
