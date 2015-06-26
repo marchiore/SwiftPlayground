286625817f5960ce43c6c96300211517a11b03e2
matheus <marchiore.matheus@gmail.com>
2015-06-25 23:19:53 -0300

    testes

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -4,15 +4,27 @@ import UIKit
 
 var str = "Hello, playground"
 
-var array = [1, 2, 3, 4]
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 func dobrar(numero: Int) -> Int{
     return numero * 2
 }
 
+//TESTANDO CLOSURE
 var novo = array.map { result in result * 2 }
 
 println(novo)
+
+
+//TESTANDO FILTRO
+var filtro = array.filter({$0 % 2 == 0 })
+
+println(filtro)
+
+
+
+
+
 
 
 
