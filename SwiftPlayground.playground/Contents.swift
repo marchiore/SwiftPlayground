ebf364aae5c6f6bc636922128f71d5674dc8f3c7
matheus <marchiore.matheus@gmail.com>
2015-06-25 23:33:10 -0300

    testes com enum

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -22,6 +22,45 @@ var filtro = array.filter({$0 % 2 == 0 })
 println(filtro)
 
 
+//TESTANDO ENUM
+enum Planet: Int {
+    case Mercury = 1, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+}
+
+let positionToFind = 3
+if let somePlanet = Planet(rawValue: positionToFind) {
+    switch somePlanet {
+    case .Earth:
+        print("Mostly harmless")
+    default:
+        print("Not a safe place for humans")
+    }
+} else {
+    print("There isn't a planet at position \(positionToFind)")
+}
+
+enum Sinaleiro: String {
+    case Amarelo = "A", Verde = "V", Vermelho = "VR"
+}
+
+let Semaforo = ""
+if let sem = Sinaleiro(rawValue: Semaforo){
+    switch sem{
+    case .Amarelo:
+        println("Amarelo")
+        
+    case .Verde:
+        println("Amarelo")
+        
+    case .Vermelho:
+        println("Vermelho")
+        
+    default:
+        println("Cor incorreta")
+    }
+}else{
+    println("Cor inexistente")
+}
 
 
 
